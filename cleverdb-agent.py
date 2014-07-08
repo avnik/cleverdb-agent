8edb76de1fda120420763c6569da9484226bff5f
Kien Pham <kien@sendgrid.com>
2014-07-08 16:12:34 -0700

    adding timeout and retry"

--- cleverdb-agent.py
+++ cleverdb-agent.py
@@ -1,74 +1,56 @@
-from subprocess import call
-from optparse import OptionParser
 import subprocess
+from time import sleep
 
 
 def run():
-    parser = OptionParser()
-    parser.add_option(
-        "-a", "--host", dest="host", type="str",
-        help="The host address. Ex: -a host.example.com"
-    )
-
-    parser.add_option(
-        "-s", "--source-port", dest="source_port", type="int",
-        help="The source port. Ex: -s 3306"
-    )
-
-    parser.add_option(
-        "-d", "--destination-port", dest="destination_port", type="int",
-        help="The destination port. Ex: -d 8080"
-    )
-
-    parser.add_option(
-        "-i", "--private-key", dest="private_key", type="str",
-        help="The path to private key file. Ex: -i ~/.ssh/my_private.key"
-    )
-
-    (options, args) = parser.parse_args()
-
-    if None in [options.host, options.source_port, options.destination_port]:
-        exit("Host name and ports are required. "
-             "Ex: -a example.com -s 3306 -d 8080")
-
-    print "Host: %s. Source port: %s. Destination port: %s" % (
-        options.host,
-        options.source_port,
-        options.destination_port)
-
-    print "Starting port forwarding..."
-    local_mapping = "%i:localhost:%i" % (options.destination_port,
-                                         options.source_port)
-    destination_mapping = options.host
     """
     -f tells ssh to go into the background (daemonize).
     -N tells ssh that you don't want to run a remote command.
     -q tells ssh to be quiet
-    -L specifies the port forwarding
+    -R remote port forwarding
     -i private key file
+
+    Ex: ssh -N -R 8080:localhost:3306 vagrant@192.168.100.3 -i key.priv
     """
-    # ssh -N -R 8080:localhost:3306 vagrant@192.168.100.3 -i key.priv
-    ssh_options = ["ssh", "-N", "-R", local_mapping, destination_mapping]
+    retry_count = 0
+    max_retry = 100
+    ssh_options = ["ssh", "-N", "-R"]
 
-    if options.private_key:
+    while True:
+        # get config from api:
+        config = _get_config()
+        local_part = "%i:localhost:%i" % (
+            config['slave_port'],
+            config['master_port'])
+        ssh_options.append(local_part)
+        ssh_options.append(config['slave_host'])
         ssh_options.append("-i")
-        # TODO: this will later come from api
-        ssh_options.append('key.priv')
+        ssh_options.append(config['private_key'])
 
-    print "SSH options..."
-    print ssh_options
-    # call_res = call(ssh_options)
-    prog = subprocess.Popen(ssh_options, stderr=subprocess.PIPE)
-    call_res = prog.communicate()[1]
+        # start the SSH tunnel
+        print "Starting ssh tunnel..."
+        prog = subprocess.Popen(ssh_options, stdout=subprocess.PIPE)
+        call_res = prog.communicate()
 
-    print 'here?...'
-    print call_res
+        # ssh tunnel broken at this point
+        if retry_count >= max_retry:
+            print "Max retry, can't open SSH tunnel"
+            break
+
+        # retry getting latest config from api:
+        retry_count += 1
+        print "Sleeping...%i seconds" % retry_count
+        sleep(retry_count)
+        print "Retrying...%i" % retry_count
 
 
-# def _get_priv_key():
-#     priv_key = open('key.priv', 'r')
-#
-#     return priv_key.read()
+def _get_config():
+    # TODO: get from api
+    return {'slave_host': "192.168.100.3",
+            'slave_port': 8080,
+            'master_port': 3306,
+            'private_key': "key.priv"}
+
 
 if __name__ == '__main__':
     run()
