d378c480ae1dbba8d5f2f90bae145bae7e6d6522
Alexander V. Nikolaev <avn@daemon.hole.ru>
2014-08-06 04:14:09 +0300

    Use namespaced package

--- setup.py
+++ setup.py
@@ -3,17 +3,20 @@
 The cleverdb-agent will open a secure tunnel to cleverdb service
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from cleverdb import __version__
+
 
 ## Requirements only for development builds using pip,
 ## This package haven't any runtime dependencies outside standard library
+## and setuptools
+requirements = ["setuptools"]
 try:
     from pip.req import parse_requirements
 except ImportError:
-    requirements = []
+    pass
 else:
-    requirements = parse_requirements("requirements.txt")
+    requirements.extend([str(ir.req) for ir in parse_requirements("requirements.txt")])
 
 doc = __doc__.splitlines()
 
@@ -22,7 +25,9 @@ setup(
     description=doc[0],
     long_description='\n'.join(doc[2:]),
     version=__version__,
-    install_requires=[str(ir.req) for ir in requirements],
+    packages=find_packages(),
+    namespace_packages=["cleverdb"],
+    install_requires=requirements,
     entry_points = """
     [console_scripts]
     cleverdb-agent=cleverdb.agent:main
