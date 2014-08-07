f2401be10736534a190564fbf69ed7d2265ca1ea
Alexander V. Nikolaev <avn@daemon.hole.ru>
2014-08-07 20:51:46 +0300

    Allow upload debs with "unstable" in release field to staging

--- fabfile.py
+++ fabfile.py
@@ -2,5 +2,6 @@ from fabric.api import run
 
 
 def deploy():
-    run('reprepro -b /srv/www/apt.cleverdb.io/ includedeb staging '
+    run('reprepro -b /srv/www/apt.cleverdb.io/ --ignore=wrongdistribution '
+        'includedeb staging '
         '/tmp/latest.deb')
